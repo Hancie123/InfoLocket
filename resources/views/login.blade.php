d45a1aefda161df9c45ef1863aa8a784e5e4537e
Hancie123 <hanciewanemphago@gmail.com>
2024-01-09 20:40:53 +0545

    ui updated

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,69 +1,118 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="en" dir="ltr">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
-    <title>InfoLocket: Secure Data Organizer</title>
-    <link rel="icon" type="image/x-icon" href="{{ url('assets/images/Logo.ico') }}" />
-    <!-- BEGIN GLOBAL MANDATORY STYLES -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-    <link href="{{ url('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ url('assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ url('assets/css/form-1.css') }}" rel="stylesheet" type="text/css" />
-    <!-- END GLOBAL MANDATORY STYLES -->
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/theme-checkbox-radio.css') }}">
-    <link rel="stylesheet" type="text/css" href="{{ url('assets/css/switches.css') }}">
-    <script src="{{ url('assets/js/jquery-3.1.1.min.js') }}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.4/toastr.min.css"/>
-    @livewireStyles
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ url('assets/css/plugin.min.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@iconscout/unicons@4.0.8/css/line.min.css">
 </head>
 
-<body class="form">
-
-
-
-
-    <div class="form-container">
-        <div class="form-form">
-            <div class="form-form-wrap">
-                <div class="form-container">
-                    <div class="form-content">
-
-                        <h1 class="">Log In to <a href="index.html"><span class="brand-name">InfoLocket</span></a>
-                        </h1>
-                        <p class="signup-link">New Here? <a href="{{url('register')}}" wire:navigate>Create an account</a></p>
-
-
-                        @livewire('login')
-
-
-                        <p class="terms-conditions">© <?php echo Date('Y'); ?> All Rights Reserved. <a
-                                href="{{url('/')}}">InfoLocket</a> is a product of Hancie Phago. <a
-                                href="javascript:void(0);">Cookie Preferences</a>, <a
-                                href="javascript:void(0);">Privacy</a>, and <a href="javascript:void(0);">Terms</a>.
-                        </p>
-
+<body>
+    <main class="main-content">
+        <div class="admin">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-xxl-3 col-xl-4 col-md-6 col-sm-8">
+                        <div class="edit-profile">
+                            <div class="edit-profile__logos">
+                                <a href="index.html">
+                                    <img class="dark"
+                                        src="{{ url('assets/img/infolocket-high-resolution-logo-transparent.png') }}"
+                                        alt>
+                                    <img class="light"
+                                        src="{{ url('assets/img/infolocket-high-resolution-logo-transparent.png') }}"
+                                        alt>
+                                </a>
+                            </div>
+                            <div class="card border-0">
+                                <div class="card-header">
+                                    <div class="edit-profile__title">
+                                        <h6>Sign in InfoLocket</h6>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <div class="edit-profile__body">
+                                        <div class="form-group mb-25">
+                                            <label for="username">Username or Email Address</label>
+                                            <input type="text" class="form-control" id="username"
+                                                placeholder="name@example.com">
+                                        </div>
+                                        <div class="form-group mb-15">
+                                            <label for="password-field">password</label>
+                                            <div class="position-relative">
+                                                <input id="password-field" type="password" class="form-control"
+                                                    name="password" placeholder="Password">
+                                                <div
+                                                    class="uil uil-eye-slash text-lighten fs-15 field-icon toggle-password2">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="admin-condition">
+                                            <div class="checkbox-theme-default custom-checkbox ">
+                                                <input class="checkbox" type="checkbox" id="check-1">
+                                                <label for="check-1">
+                                                    <span class="checkbox-text">Keep me logged in</span>
+                                                </label>
+                                            </div>
+                                            <a href="forget-password.html">forget password?</a>
+                                        </div>
+                                        <div
+                                            class="admin__button-group button-group d-flex pt-1 justify-content-md-start justify-content-center">
+                                            <button
+                                                class="btn btn-primary btn-default w-100 btn-squared text-capitalize lh-normal px-50 signIn-createBtn ">
+                                                sign in
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                <div class="admin-topbar">
+                                    <p class="mb-0">
+                                        Don't have an account?
+                                        <a href="sign-up.html" class="color-primary">
+                                            Sign up
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="form-image">
-            <div class="l-image">
+    </main>
+    <div id="overlayer">
+        <div class="loader-overlay">
+            <div class="dm-spin-dots spin-lg">
+                <span class="spin-dot badge-dot dot-primary"></span>
+                <span class="spin-dot badge-dot dot-primary"></span>
+                <span class="spin-dot badge-dot dot-primary"></span>
+                <span class="spin-dot badge-dot dot-primary"></span>
             </div>
         </div>
     </div>
+    <div class="enable-dark-mode dark-trigger">
+        <ul>
+            <li>
+                <a href="#">
+                    <i class="uil uil-moon"></i>
+                </a>
+            </li>
+        </ul>
+    </div>
 
+    <script src="{{ url('assets/js/plugins.min.js') }}"></script>
+    <script src="{{ url('assets/js/script.min.js') }}"></script>
 
-
-    <script src="{{ url('assets/js/popper.min.js') }}"></script>
-    <script src="{{ url('assets/js/bootstrap.min.js') }}"></script>
-
-    <!-- END GLOBAL MANDATORY SCRIPTS -->
-    <script src="{{ url('assets/js/form-1.js') }}"></script>
-@livewireScripts
 </body>
 
 </html>
